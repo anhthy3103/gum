6b3ccf979051e3d870abeb6fdd32a9d65df87057
Thy <anhthy3103@gmail.com>
2016-11-04 12:43:47 +0700

    init kjh

---
+++ abc/src/abc/my.java
@@ -0,0 +1,29 @@
+package abc;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class my extends JFrame {
+	public my(){
+		setSize(400,400);
+		setLayout(null);
+		setTitle("git");
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		my f= new my();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
